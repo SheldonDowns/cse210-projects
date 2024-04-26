cb4f43ee8b487e5327dd1c6bbcd783586b4ddf85
SheldonDowns <sheldondowns2002@gmail.com>
2024-04-26 14:40:55 -0600

    Prep 2 finished

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,39 @@
 using System;
 
-class Program
+Console.Write("What is your grade? ");
+string grade_input = Console.ReadLine();
+int grade = int.Parse(grade_input);
+
+string letter; // Declare letter outside of the if blocks
+
+if (grade >= 90)
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+    letter = "A";
+}
+else if (grade >= 80)
+{
+    letter = "B";
+}
+else if (grade >= 70)
+{
+    letter = "C";
+}
+else if (grade >= 60)
+{
+    letter = "D";
+}
+else
+{
+    letter = "F";
+}
+
+Console.WriteLine($"Your grade is {letter}");
+
+if (grade >= 70)
+{
+    Console.WriteLine("Congratulations you passed!");
+}
+else 
+{
+    Console.WriteLine("Better luck next time");
 }
