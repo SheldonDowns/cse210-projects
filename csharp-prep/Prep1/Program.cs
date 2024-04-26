1c746dbb34d59e20fc33d95472b5746f17841f9c
SheldonDowns <sheldondowns2002@gmail.com>
2024-04-26 14:02:07 -0600

    Finished prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,9 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
-    }
-}
+Console.Write("Whats your first name? ");
+string fname = Console.ReadLine();
+Console.Write("What is your last name? ");
+string lname = Console.ReadLine();
+
+
+Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
