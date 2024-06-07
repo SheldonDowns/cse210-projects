32d4a54072208495fd00ac062f230f8a9c453be8
SheldonDowns <sheldondowns2002@gmail.com>
2024-06-07 15:06:24 -0600

    learn04

---
+++ prepare/Learning04/Math.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Net;
+
+class Math : Assignment
+{
+    private string _textbookSection;
+    private string _problems;
+
+    public Math(string studentName, string topic, string textbookSection, string problems)
+        : base(studentName, topic)
+    {
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
+    
+  public string GetHomeworkList()
+    {
+        return $"Section {_textbookSection} Problems {_problems}";
+    }
+}
